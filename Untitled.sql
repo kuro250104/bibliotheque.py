f792e9020aed90da62e22b1ed97f265eb2d4b560
Ota <q.tonellato@free.fr>
2024-07-16 15:11:51 +0200

    modif BDD auto increment & ajout name dans Item

--- Untitled.sql
+++ Untitled.sql
@@ -1,5 +1,13 @@
+# DROP TABLE IF EXISTS `Item`;
+# DROP TABLE IF EXISTS `Type`;
+# DROP TABLE IF EXISTS `Emprunt`;
+# DROP TABLE IF EXISTS `Genre`;
+# DROP TABLE IF EXISTS `Auteur`;
+# DROP TABLE IF EXISTS `Utilisateur`;
+
 CREATE TABLE `Item` (
-  `id` integer PRIMARY KEY,
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(50),
   `type_id` integer,
   `auteur_id` integer,
   `genre_id` integer,
@@ -8,12 +16,12 @@ CREATE TABLE `Item` (
 );
 
 CREATE TABLE `Type` (
-  `id` integer PRIMARY KEY,
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
   `number` integer
 );
 
 CREATE TABLE `Emprunt` (
-  `id` integer PRIMARY KEY,
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
   `emprunt` bool,
   `date_debut` date,
   `date_fin` date,
@@ -21,30 +29,26 @@ CREATE TABLE `Emprunt` (
 );
 
 CREATE TABLE `Genre` (
-  `id` integer PRIMARY KEY,
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
   `nom` varchar(50),
   `prenom` varchar(50)
 );
 
 CREATE TABLE `Auteur` (
-  `id` integer PRIMARY KEY,
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
   `nom` varchar(50),
   `prenom` varchar(50)
 );
 
 CREATE TABLE `Utilisateur` (
-  `id` integer PRIMARY KEY,
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
   `nom` varchar(50),
   `prenom` varchar(50),
   `created_at` timestamp
 );
 
 ALTER TABLE `Item` ADD FOREIGN KEY (`type_id`) REFERENCES `Type` (`id`);
-
 ALTER TABLE `Item` ADD FOREIGN KEY (`auteur_id`) REFERENCES `Auteur` (`id`);
-
 ALTER TABLE `Item` ADD FOREIGN KEY (`genre_id`) REFERENCES `Genre` (`id`);
-
 ALTER TABLE `Item` ADD FOREIGN KEY (`emprunt_id`) REFERENCES `Emprunt` (`id`);
-
-ALTER TABLE `Emprunt` ADD FOREIGN KEY (`user_id`) REFERENCES `Utilisateur` (`id`);/*END*/
+ALTER TABLE `Emprunt` ADD FOREIGN KEY (`user_id`) REFERENCES `Utilisateur` (`id`);
