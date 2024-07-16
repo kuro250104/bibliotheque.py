07cecb1b63f9ac3fbbb2483780065c22e8eb29bc
Kuro <45374906+kuro250104@users.noreply.github.com>
2024-07-16 15:54:38 +0200

    add user

--- Class/User.py
+++ Class/User.py
@@ -1,6 +1,27 @@
+import mysql.connector
+from datetime import date
+from mysql.connector import Error
 import BDD
 
-class User(): 
-    
-    def __init__(self) -> None:
-        pass
+class Utilisateur:
+    def __init__(self, id, nom, prenom, adresse,):
+        self.id = id
+        self.nom = nom
+        self.prenom = prenom
+        self.adresse = adresse
+
+
+    def ajouter(self, bdd_acces):
+        try:
+            cursor = bdd_acces.Connect.cursor()
+            sql = """
+            INSERT INTO utilisateur (nom, prenom, adresse)
+            VALUES (%s, %s, %s)
+            """
+            val = (self.nom, self.prenom, self.adresse, )
+            cursor.execute(sql, val)
+            bdd_acces.Connect.commit()
+            print(f"Utilisateur {self.nom} {self.prenom} ajouté avec succès")
+        except Error as e:
+            print(f"Erreur lors de l'ajout de l'utilisateur : {e}")
+
--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-from Class.Bibliotheque import Bibliotheque
+import Class.Bibliotheque as Bibliotheque
+from Class.User import Utilisateur
 from BDD import BDD_Acces
+from uuid import uuid4
 
 
-BDD_Biblio = BDD_Acces("B2B2", "B2B2", "localhost", "bibliotheque")
+#test = Bibliotheque()
 
-BDD_Biblio.Init_DB()
+# BDD_Biblio.Init_DB()
 
-Biblio = Bibliotheque(BDD_Biblio)
+# main.py
+
+
+if __name__ == "__main__":
+    bdd_acces = BDD_Acces("B2B2", "B2B2", "localhost", "bibliotheque")
+    bdd_acces.Init_DB()
+
+    id_utilisateur = str(uuid4())
+    nom_utilisateur = input("Entrez le nom de l'utilisateur : ")
+    prenom_utilisateur = input("Entrez le prénom de l'utilisateur : ")
+    adresse_utilisateur = input("Entrez l'adresse de l'utilisateur : ")
+
+    utilisateur = Utilisateur(id=id_utilisateur, nom=nom_utilisateur, prenom=prenom_utilisateur,
+                              adresse=adresse_utilisateur)
+    utilisateur.ajouter(bdd_acces)
+
+    utilisateur.id = 1
+    utilisateur.adresse = "456 Rue Secondaire"
+    bdd_acces.Connect.close()
